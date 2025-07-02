e44e797c54071c53903b49ec7bcc9bab6a2ca569
Alexey Tukin <tukin2k@gmail.com>
2025-07-02 18:02:20 +0300

    add_attach

--- conftest.py
+++ conftest.py
@@ -6,6 +6,7 @@ from selene import browser
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from utils import attach
 
 @pytest.fixture(scope="session")
 def browser_screen_size():
@@ -54,9 +55,9 @@ def setup_browser(request):
     browser.config.window_height = 1080
     yield browser
 
-    # attach.add_screenshot(browser)
+    attach.add_screenshot(browser)
     # attach.add_logs(browser)
-    # attach.add_html(browser)
-    # attach.add_video(browser)
+    attach.add_html(browser)
+    attach.add_video(browser)
 
     browser.quit()
