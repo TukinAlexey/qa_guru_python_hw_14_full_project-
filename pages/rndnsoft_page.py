ed47d5667d707d10d8d07dbaff45f8c6612a953f
Alexey Tukin <tukin2k@gmail.com>
2025-09-01 12:05:14 +0300

    refactor_tests

--- pages/rndnsoft_page.py
+++ pages/rndnsoft_page.py
@@ -1,109 +1,75 @@
-from time import sleep
-
 from selene import have, be
 
-from conftest import setup_browser
 
+class RndSoftPage:
+    def __init__(self, browser):
+        self.browser = browser
 
-class Rndsoft_page:
-    browser = setup_browser
-
-    def open_start_pag(self, setup_browser):
-        browser = setup_browser
-
-        browser.open('https://www.rnd-soft.ru/')
-        browser.element('[class="button is-primary"]').click()
-        browser.element('[class="has-text-white-ter has-text-weight-light"]').should(have.exact_text(
+    def open_start_page(self):
+        self.browser.open('https://www.rnd-soft.ru/')
+        self.browser.element('[class="button is-primary"]').click()
+        self.browser.element('[class="has-text-white-ter has-text-weight-light"]').should(have.exact_text(
             "Мы создаём продукты и проекты. Помогаем нашим клиентам в достижении успеха Присоединяйтесь к команде или пишите для сотрудничества."))
 
-    def change_language(self, setup_browser):
-        browser = setup_browser
+    def change_language(self):
+        self.browser.element('[data-language-switcher-locale-param="ru"]').click()
+        self.browser.element('[data-language-switcher-locale-param="en"]').click()
 
-        browser.element('[data-language-switcher-locale-param="ru"]').click()
-        browser.element('[data-language-switcher-locale-param="en"]').click()
-        sleep(2)
-
-    def check_english_language_on_page(self, setup_browser):
-        browser = setup_browser
-
-        browser.element('[class="has-text-white-ter has-text-weight-light"]').should(have.exact_text(
+    def check_english_language_on_page(self):
+        self.browser.element('[class="has-text-white-ter has-text-weight-light"]').should(have.exact_text(
             "We create products and projects. We help our clients succeed Join the team or write to us for cooperation."))
 
-    def requisites_page(self, setup_browser):
-        browser = setup_browser
+    def requisites_page(self):
+        self.browser.element('[class="requisites is-size-5 link link--grey-dark"]').click()
 
-        browser.element('[class="requisites is-size-5 link link--grey-dark"]').click()
-        sleep(2)
-
-    def check_inn(self, setup_browser):
-        browser = setup_browser
-
-        browser.element('[class="text has-text-grey-dark custom-is-size-body line-height-140"]').should(have.text(
+    def check_inn(self):
+        self.browser.element('[class="text has-text-grey-dark custom-is-size-body line-height-140"]').should(have.text(
             "6165189197"))
 
-    def go_to_product_page(self, setup_browser):
-        browser = setup_browser
+    def go_to_product_page(self):
+        self.browser.element('[data-action="click->burger-menu#toggleBurgerMenu"]').click()
+        self.browser.element('//*[contains(text(),"Продукты")]').click()
 
-        browser.element('[data-action="click->burger-menu#toggleBurgerMenu"]').click()
-        browser.element('//*[contains(text(),"Продукты")]').click()
-
-    def check_product_page(self, setup_browser):
-        browser = setup_browser
-
-        browser.element('.products .is-hidden-mobile h1').should(have.exact_text(
+    def check_product_page(self):
+        self.browser.element('.products .is-hidden-mobile h1').should(have.exact_text(
             "Продукты"))
 
-    def go_to_vacancy_page(self, setup_browser):
-        browser = setup_browser
+    def go_to_vacancy_page(self):
+        self.browser.element('[data-action="click->burger-menu#toggleBurgerMenu"]').click()
+        self.browser.element('//*[contains(text(),"Вакансии")]').click()
 
-        browser.element('[data-action="click->burger-menu#toggleBurgerMenu"]').click()
-        browser.element('//*[contains(text(),"Вакансии")]').click()
-
-    def check_availability_qa_vacancy(self, setup_browser):
-        browser = setup_browser
-
-        browser.element('#vacancy_8 > .name.is-flex.is-align-items-center.is-justify-content-space-between > p').should(
+    def check_availability_qa_vacancy(self):
+        self.browser.element(
+            '#vacancy_8 > .name.is-flex.is-align-items-center.is-justify-content-space-between > p').should(
             have.exact_text(
                 "QA-инженер/Тестировщик"))
 
-    def check_description_qa_vacancy(self, setup_browser):
-        browser = setup_browser
-
-        browser.element(
+    def check_description_qa_vacancy(self):
+        self.browser.element(
             '[class="is-size-5-tablet custom-is-size-body--mobile has-text-white is-family-sans-serif line-height-140"]').should(
             have.exact_text(
                 "Мы не стоим на месте: используем современные подходы к разработке, заботимся о безопасности, ценим гибкость и умеем работать командой. И сейчас ищем того, кто усилит нас в роли QA инженера."))
 
-    def go_to_company_page(self, setup_browser):
-        browser = setup_browser
+    def go_to_company_page(self):
+        self.browser.element('[data-action="click->burger-menu#toggleBurgerMenu"]').click()
+        self.browser.element('//*[contains(text(),"Компания")]').click()
 
-        browser.element('[data-action="click->burger-menu#toggleBurgerMenu"]').click()
-        browser.element('//*[contains(text(),"Компания")]').click()
+    def fill_and_submit_message_form(self):
+        self.browser.element('[placeholder="Имя*"]').type('Ivan')
+        self.browser.element('[placeholder="Электронная почта*"]').type('dobryden@mail.ru')
+        self.browser.element('[placeholder="Сообщение*"]').type('Добрый день')
+        self.browser.element('#feedback_agreement').click()
+        self.browser.element('[type="submit"]').click()
 
-    def fill_and_submit_message_form(self, setup_browser):
-        browser = setup_browser
+    def check_sending_message_success(self):
+        self.browser.element('//h2[text()="Отправлено!"]').should(be.visible)
 
-        browser.element('[placeholder="Имя*"]').type('Ivan')
-        browser.element('[placeholder="Электронная почта*"]').type('dobryden@mail.ru')
-        browser.element('[placeholder="Сообщение*"]').type('Добрый день')
-        browser.element('#feedback_agreement').click()
-        browser.element('[type="submit"]').click()
+    def fill_and_submit_message_form_without_agreement(self):
+        self.browser.element('[placeholder="Имя*"]').type('Ivan')
+        self.browser.element('[placeholder="Электронная почта*"]').type('dobryden@mail.ru')
+        self.browser.element('[placeholder="Сообщение*"]').type('Добрый день')
+        self.browser.element('[type="submit"]').click()
 
-    def check_sending_message_success(self, setup_browser):
-        browser = setup_browser
-
-        browser.element('//h2[text()="Отправлено!"]').should(be.visible)
-
-    def fill_and_submit_message_form_without_agreement(self, setup_browser):
-        browser = setup_browser
-
-        browser.element('[placeholder="Имя*"]').type('Ivan')
-        browser.element('[placeholder="Электронная почта*"]').type('dobryden@mail.ru')
-        browser.element('[placeholder="Сообщение*"]').type('Добрый день')
-        browser.element('[type="submit"]').click()
-
-    def check_error_sending_message(self, setup_browser):
-        browser = setup_browser
-
-        browser.element('[class="is-size-6 line-height-120 has-text-danger mt-1"]').should(have.exact_text(
+    def check_error_sending_message(self):
+        self.browser.element('[class="is-size-6 line-height-120 has-text-danger mt-1"]').should(have.exact_text(
             "Чекбокс не может быть неотмеченным"))
--- tests/test_full_project.py
+++ tests/test_full_project.py
@@ -1,8 +1,7 @@
 import allure
 from allure_commons.types import Severity
-
 from conftest import setup_browser
-from pages.rndnsoft_page import Rndsoft_page
+from pages.rndnsoft_page import RndSoftPage
 
 
 @allure.tag("test")
@@ -12,17 +11,13 @@ from pages.rndnsoft_page import Rndsoft_page
 @allure.story('Проверка смены на английский язык')
 @allure.link('https://github.com', name='Testing')
 def test_change_english_language(setup_browser):
-    browser = setup_browser
-    rndsoft_page = Rndsoft_page()
-
+    rndsoft_page = RndSoftPage(setup_browser)
     with allure.step("Open start page"):
-        rndsoft_page.open_start_pag(browser)
-
+        rndsoft_page.open_start_page()
     with allure.step("change language"):
-        rndsoft_page.change_language(browser)
-
+        rndsoft_page.change_language()
     with allure.step("check english language on page"):
-        rndsoft_page.check_english_language_on_page(browser)
+        rndsoft_page.check_english_language_on_page()
 
 
 @allure.tag("test")
@@ -32,17 +27,13 @@ def test_change_english_language(setup_browser):
 @allure.story('Проверка ИНН на странице реквизитов')
 @allure.link('https://github.com', name='Testing')
 def test_inn_on_requisites_page(setup_browser):
-    browser = setup_browser
-    rndsoft_page = Rndsoft_page()
-
+    rndsoft_page = RndSoftPage(setup_browser)
     with allure.step("Open start page"):
-        rndsoft_page.open_start_pag(browser)
-
+        rndsoft_page.open_start_page()
     with allure.step("go to requisites page"):
-        rndsoft_page.requisites_page(browser)
-
+        rndsoft_page.requisites_page()
     with allure.step("check inn on requisites page"):
-        rndsoft_page.check_inn(browser)
+        rndsoft_page.check_inn()
 
 
 @allure.tag("test")
@@ -52,17 +43,13 @@ def test_inn_on_requisites_page(setup_browser):
 @allure.story('Проверка перехода на страницу Продукты')
 @allure.link('https://github.com', name='Testing')
 def test_go_to_product_page(setup_browser):
-    browser = setup_browser
-    rndsoft_page = Rndsoft_page()
-
+    rndsoft_page = RndSoftPage(setup_browser)
     with allure.step("Open start page"):
-        rndsoft_page.open_start_pag(browser)
-
+        rndsoft_page.open_start_page()
     with allure.step("go to product page"):
-        rndsoft_page.go_to_product_page(browser)
-
+        rndsoft_page.go_to_product_page()
     with allure.step("check product page"):
-        rndsoft_page.check_product_page(browser)
+        rndsoft_page.check_product_page()
 
 
 @allure.tag("test")
@@ -72,17 +59,13 @@ def test_go_to_product_page(setup_browser):
 @allure.story('Проверка наличия вакансии')
 @allure.link('https://github.com', name='Testing')
 def test_availability_qa_vacancy(setup_browser):
-    browser = setup_browser
-    rndsoft_page = Rndsoft_page()
-
+    rndsoft_page = RndSoftPage(setup_browser)
     with allure.step("Open start page"):
-        rndsoft_page.open_start_pag(browser)
-
+        rndsoft_page.open_start_page()
     with allure.step("go to product page"):
-        rndsoft_page.go_to_vacancy_page(browser)
-
+        rndsoft_page.go_to_vacancy_page()
     with allure.step("check product page"):
-        rndsoft_page.check_availability_qa_vacancy(browser)
+        rndsoft_page.check_availability_qa_vacancy()
 
 
 @allure.tag("test")
@@ -92,17 +75,13 @@ def test_availability_qa_vacancy(setup_browser):
 @allure.story('Проверка корректного описания вакансии')
 @allure.link('https://github.com', name='Testing')
 def test_description_qa_vacancy(setup_browser):
-    browser = setup_browser
-    rndsoft_page = Rndsoft_page()
-
+    rndsoft_page = RndSoftPage(setup_browser)
     with allure.step("Open start page"):
-        rndsoft_page.open_start_pag(browser)
-
+        rndsoft_page.open_start_page()
     with allure.step("go to product page"):
-        rndsoft_page.go_to_vacancy_page(browser)
-
+        rndsoft_page.go_to_vacancy_page()
     with allure.step("check product page"):
-        rndsoft_page.check_description_qa_vacancy(browser)
+        rndsoft_page.check_description_qa_vacancy()
 
 
 @allure.tag("test")
@@ -112,20 +91,15 @@ def test_description_qa_vacancy(setup_browser):
 @allure.story('Проверка отправки сообщения со страницы "Компания"')
 @allure.link('https://github.com', name='Testing')
 def test_sending_message(setup_browser):
-    browser = setup_browser
-    rndsoft_page = Rndsoft_page()
-
+    rndsoft_page = RndSoftPage(setup_browser)
     with allure.step("Open start page"):
-        rndsoft_page.open_start_pag(browser)
-
+        rndsoft_page.open_start_page()
     with allure.step("go to company page"):
-        rndsoft_page.go_to_company_page(browser)
-
+        rndsoft_page.go_to_company_page()
     with allure.step("fill massage form and submit message"):
-        rndsoft_page.fill_and_submit_message_form(browser)
-
+        rndsoft_page.fill_and_submit_message_form()
     with allure.step("check sending message success"):
-        rndsoft_page.check_sending_message_success(browser)
+        rndsoft_page.check_sending_message_success()
 
 
 @allure.tag("test")
@@ -136,17 +110,12 @@ def test_sending_message(setup_browser):
     'Проверка отправки сообщения со страницы "Компания" без предоставления согласия с Политикой конфиденциальности')
 @allure.link('https://github.com', name='Testing')
 def test_sending_message_without_agreement(setup_browser):
-    browser = setup_browser
-    rndsoft_page = Rndsoft_page()
-
+    rndsoft_page = RndSoftPage(setup_browser)
     with allure.step("Open start page"):
-        rndsoft_page.open_start_pag(browser)
-
+        rndsoft_page.open_start_page()
     with allure.step("go to company page"):
-        rndsoft_page.go_to_company_page(browser)
-
+        rndsoft_page.go_to_company_page()
     with allure.step("fill massage form and submit message without agreement"):
-        rndsoft_page.fill_and_submit_message_form_without_agreement(browser)
-
+        rndsoft_page.fill_and_submit_message_form_without_agreement()
     with allure.step("check error sending message"):
-        rndsoft_page.check_error_sending_message(browser)
+        rndsoft_page.check_error_sending_message()
