e1afe5258bf57ca34b37e6de2f9de3087f91b477
Alexey Tukin <tukin2k@gmail.com>
2025-09-01 12:12:39 +0300

    refactor_tests

--- tests/test_full_project.py
+++ tests/test_full_project.py
@@ -11,13 +11,13 @@ from pages.rndnsoft_page import RndSoftPage
 @allure.story('Проверка смены на английский язык')
 @allure.link('https://github.com', name='Testing')
 def test_change_english_language(setup_browser):
-    rndsoft_page = RndSoftPage(setup_browser)
+    rspage = RndSoftPage(setup_browser)
     with allure.step("Open start page"):
-        rndsoft_page.open_start_page()
+        rspage.open_start_page()
     with allure.step("change language"):
-        rndsoft_page.change_language()
+        rspage.change_language()
     with allure.step("check english language on page"):
-        rndsoft_page.check_english_language_on_page()
+        rspage.check_english_language_on_page()
 
 
 @allure.tag("test")
@@ -27,13 +27,13 @@ def test_change_english_language(setup_browser):
 @allure.story('Проверка ИНН на странице реквизитов')
 @allure.link('https://github.com', name='Testing')
 def test_inn_on_requisites_page(setup_browser):
-    rndsoft_page = RndSoftPage(setup_browser)
+    rspage = RndSoftPage(setup_browser)
     with allure.step("Open start page"):
-        rndsoft_page.open_start_page()
+        rspage.open_start_page()
     with allure.step("go to requisites page"):
-        rndsoft_page.requisites_page()
+        rspage.requisites_page()
     with allure.step("check inn on requisites page"):
-        rndsoft_page.check_inn()
+        rspage.check_inn()
 
 
 @allure.tag("test")
@@ -43,13 +43,13 @@ def test_inn_on_requisites_page(setup_browser):
 @allure.story('Проверка перехода на страницу Продукты')
 @allure.link('https://github.com', name='Testing')
 def test_go_to_product_page(setup_browser):
-    rndsoft_page = RndSoftPage(setup_browser)
+    rspage = RndSoftPage(setup_browser)
     with allure.step("Open start page"):
-        rndsoft_page.open_start_page()
+        rspage.open_start_page()
     with allure.step("go to product page"):
-        rndsoft_page.go_to_product_page()
+        rspage.go_to_product_page()
     with allure.step("check product page"):
-        rndsoft_page.check_product_page()
+        rspage.check_product_page()
 
 
 @allure.tag("test")
@@ -59,13 +59,13 @@ def test_go_to_product_page(setup_browser):
 @allure.story('Проверка наличия вакансии')
 @allure.link('https://github.com', name='Testing')
 def test_availability_qa_vacancy(setup_browser):
-    rndsoft_page = RndSoftPage(setup_browser)
+    rspage = RndSoftPage(setup_browser)
     with allure.step("Open start page"):
-        rndsoft_page.open_start_page()
+        rspage.open_start_page()
     with allure.step("go to product page"):
-        rndsoft_page.go_to_vacancy_page()
+        rspage.go_to_vacancy_page()
     with allure.step("check product page"):
-        rndsoft_page.check_availability_qa_vacancy()
+        rspage.check_availability_qa_vacancy()
 
 
 @allure.tag("test")
@@ -75,13 +75,13 @@ def test_availability_qa_vacancy(setup_browser):
 @allure.story('Проверка корректного описания вакансии')
 @allure.link('https://github.com', name='Testing')
 def test_description_qa_vacancy(setup_browser):
-    rndsoft_page = RndSoftPage(setup_browser)
+    rspage = RndSoftPage(setup_browser)
     with allure.step("Open start page"):
-        rndsoft_page.open_start_page()
+        rspage.open_start_page()
     with allure.step("go to product page"):
-        rndsoft_page.go_to_vacancy_page()
+        rspage.go_to_vacancy_page()
     with allure.step("check product page"):
-        rndsoft_page.check_description_qa_vacancy()
+        rspage.check_description_qa_vacancy()
 
 
 @allure.tag("test")
@@ -91,15 +91,15 @@ def test_description_qa_vacancy(setup_browser):
 @allure.story('Проверка отправки сообщения со страницы "Компания"')
 @allure.link('https://github.com', name='Testing')
 def test_sending_message(setup_browser):
-    rndsoft_page = RndSoftPage(setup_browser)
+    rspage = RndSoftPage(setup_browser)
     with allure.step("Open start page"):
-        rndsoft_page.open_start_page()
+        rspage.open_start_page()
     with allure.step("go to company page"):
-        rndsoft_page.go_to_company_page()
+        rspage.go_to_company_page()
     with allure.step("fill massage form and submit message"):
-        rndsoft_page.fill_and_submit_message_form()
+        rspage.fill_and_submit_message_form()
     with allure.step("check sending message success"):
-        rndsoft_page.check_sending_message_success()
+        rspage.check_sending_message_success()
 
 
 @allure.tag("test")
@@ -110,12 +110,12 @@ def test_sending_message(setup_browser):
     'Проверка отправки сообщения со страницы "Компания" без предоставления согласия с Политикой конфиденциальности')
 @allure.link('https://github.com', name='Testing')
 def test_sending_message_without_agreement(setup_browser):
-    rndsoft_page = RndSoftPage(setup_browser)
+    rspage = RndSoftPage(setup_browser)
     with allure.step("Open start page"):
-        rndsoft_page.open_start_page()
+        rspage.open_start_page()
     with allure.step("go to company page"):
-        rndsoft_page.go_to_company_page()
+        rspage.go_to_company_page()
     with allure.step("fill massage form and submit message without agreement"):
-        rndsoft_page.fill_and_submit_message_form_without_agreement()
+        rspage.fill_and_submit_message_form_without_agreement()
     with allure.step("check error sending message"):
-        rndsoft_page.check_error_sending_message()
+        rspage.check_error_sending_message()
